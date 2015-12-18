cb570f8d3964fa6db528d2f60412ab2a392250cd
Malcolm Macrae-Gibson <[A[Dcapitalkind>
2015-12-18 12:11:15 -0500

    adds groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,4 +5,36 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(group_params)
+    redirect_to groups_path
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+ def update
+    group = Group.find(params[:id])
+    group.update(group_params)
+    redirect_to groups_path
+  end
+
+  def destroy
+    group = Group.find(params[:id])
+    group.delete
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:guest_count, :paid_for, :employee_id)
+
+  end
+
 end
