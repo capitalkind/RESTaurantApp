867e201e1d56f198fdf73876b3b4d9d8ce05cab8
Malcolm Macrae-Gibson <[A[Dcapitalkind>
2015-12-20 23:28:16 -0500

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Employee.create(username: 'Malcolm', password: 'malcolm', permission: 3)
+Employee.create(username: 'Steve', password: 'steve', permission: 2)
+Employee.create(username: 'Tim', password: 'tim', permission: 2)
+Employee.create(username: 'Bill', password: 'bill', permission: 1)
+
+Food.create(name: 'Chicken Parm', description: 'Chickeny and Delicious', price: 10)
+Food.create(name: 'The Big Salad', description: 'We will hand it to you', price: 11)
+Food.create(name: 'Thai Chicken', description: 'Just Spicy Enough', price: 9)
+Food.create(name: 'Signature Pasta', description: 'Homemade Sauce!', price: 12)
